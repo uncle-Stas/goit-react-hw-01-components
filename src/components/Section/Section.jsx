a2173aeb3319d55a33104dc37307bf5fac66109c
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-09-15 18:15:14 +0300

    first task complite without style

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,7 +1,19 @@
+import css from './Section.module.css';
+import { Profile } from 'components/Profile/Profile';
+
+import user from 'user';
+
 export const Section = props => (
-  <section>
-    <div>
-      <h1>{props.sectionTitle}</h1>
+  <section className={css.section}>
+    <div className={css.container}>
+      <h2 className={css.title}>{props.sectionTitle}</h2>
+      <Profile
+        userName={user.username}
+        tag={user.tag}
+        avatar={user.avatar}
+        location={user.location}
+        stats={user.stats}
+      />
     </div>
   </section>
 );
