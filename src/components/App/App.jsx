68a17f5c0fd53a9bc12c759a1a9180d9c3b0ef43
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-09-15 16:45:52 +0300

    start work

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import { Section } from 'components/Section/Section';
+
+export const App = () => {
+  return (
+    <>
+      <Section sectionTitle="Profile" />
+    </>
+  );
+};
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,7 @@
+export const Section = props => (
+  <section>
+    <div>
+      <h1>{props.sectionTitle}</h1>
+    </div>
+  </section>
+);
