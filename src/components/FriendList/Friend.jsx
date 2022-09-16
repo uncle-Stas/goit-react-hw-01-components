58189acb00a559d6b6125b1fbbddebe36c58ad66
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-09-16 23:30:34 +0300

    fixed console errors

--- src/components/FriendList/Friend.jsx
+++ src/components/FriendList/Friend.jsx
@@ -1,10 +1,10 @@
 import css from './Friend.module.css';
 
-export const Friend = ({ key, name, avatar, isOnline }) => {
+export const Friend = ({ name, avatar, isOnline }) => {
   const status = isOnline ? 'online' : 'offline';
 
   return (
-    <li key={key} className={css.friendItem}>
+    <li className={css.friendItem}>
       <span className={`${css.status} ${css[status]}`}></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
--- src/components/Statistic/StatsItem.jsx
+++ src/components/Statistic/StatsItem.jsx
@@ -1,13 +1,9 @@
 import css from './StatsItem.module.css';
 import { getRandomHexColor } from 'utils/getRandomHexColor';
 
-export const StatsItem = ({ key, label, percentage }) => {
+export const StatsItem = ({ label, percentage }) => {
   return (
-    <li
-      key={key}
-      className={css.item}
-      style={{ backgroundColor: getRandomHexColor() }}
-    >
+    <li className={css.item} style={{ backgroundColor: getRandomHexColor() }}>
       <span className={css.label}>{label}</span>
       <span className={css.percentage}>{percentage}%</span>
     </li>
